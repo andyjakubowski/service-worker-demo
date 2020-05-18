be2020f149e740ba02ac1d1fb6d7c128d5922cdc
Andy Jakubowski <hello@andyjakubowski.com>
2020-05-18 18:00:58 +0200

    Updates

---
+++ app.js
@@ -0,0 +1,88 @@
+const images = [
+  {
+    url: "images/sunflower.jpg",
+    alt: "Sunflower",
+    author: "Brigitte Tohm",
+  },
+  {
+    url: "images/icyWaterfall.jpg",
+    alt: "Icy Waterfall",
+    author: "Rémy Penet",
+  },
+  {
+    url: "images/cherryBlossom.jpg",
+    alt: "Cherry Blossom",
+    author: "The Miscellanista",
+  },
+  {
+    url: "images/leaves.jpg",
+    alt: "Leaves",
+    author: "Vino Li",
+  },
+];
+
+function loadImage(imageJSON) {
+  return new Promise((resolve, reject) => {
+    fetch(imageJSON.url).then((response) => {
+      if (response.status == 200) {
+        const arrayResponse = [];
+        response.blob().then((imageBlob) => {
+          arrayResponse[0] = imageBlob;
+          arrayResponse[1] = imageJSON;
+          resolve(arrayResponse);
+        });
+      } else {
+        reject(
+          Error(
+            `Image did not load successfully; error code: ${response.status} ${response.statusText}`
+          )
+        );
+      }
+    });
+  });
+}
+
+function runApp() {
+  const main = document.querySelector("main");
+  images.forEach((image) => {
+    loadImage(image).then((arrayResponse) => {
+      const figure = document.createElement("figure");
+      const img = document.createElement("img");
+      const figcaption = document.createElement("figcaption");
+      const imageURL = window.URL.createObjectURL(arrayResponse[0]);
+      img.src = imageURL;
+      img.alt = arrayResponse[1].alt;
+      figcaption.innerText = `${arrayResponse[1].alt} by ${arrayResponse[1].author}`;
+      figure.append(img, figcaption);
+      main.append(figure);
+    });
+  });
+}
+
+if ("serviceWorker" in navigator) {
+  navigator.serviceWorker
+    .register("/src/serviceWorker.js", { scope: "/src/" })
+    .then((registration) => {
+      console.log(
+        "Successful Service Worker registration. Scope:",
+        registration.scope
+      );
+
+      if (registration.installing) {
+        console.log("Service worker installing");
+      } else if (registration.waiting) {
+        console.log("Service worker installed");
+      } else if (registration.active) {
+        console.log("Service worker active");
+      }
+    })
+    .catch((error) => {
+      console.log("Service Worker registration failed:", error);
+    });
+}
+
+if (document.readyState !== "loading") {
+  runApp();
+} else {
+  document.addEventListener("DOMContentLoaded", runApp);
+}
---
+++ serviceWorker.js
@@ -0,0 +1,51 @@
+const path = "/src";
+
+self.addEventListener("install", (event) => {
+  console.log("[🤖SW] install fired.");
+  event.waitUntil(
+    caches.open("v1").then((cache) => {
+      const items = [
+        `${path}/`,
+        `${path}/index.html`,
+        `${path}/styles.css`,
+        `${path}/app.js`,
+        `${path}/images/sunflower.jpg`,
+        `${path}/images/icyWaterfall.jpg`,
+        `${path}/images/cherryBlossom.jpg`,
+        `${path}/images/leaves.jpg`,
+      ];
+      console.log("Opened cache. Adding items:", items);
+      return cache.addAll(items);
+    })
+  );
+});
+
+self.addEventListener("activate", (event) => {
+  console.log("[🤖SW] activate fired.");
+});
+
+self.addEventListener("message", (event) => {
+  console.log("[🤖SW] message fired.");
+  console.log(e);
+});
+
+self.addEventListener("fetch", (event) => {
+  console.log("[🤖SW] fetch fired:", event.request.url);
+  event.respondWith(
+    caches.match(event.request).then((response) => {
+      if (response) {
+        return response;
+      } else {
+        return fetch(event.request)
+          .then((response) => {
+            const responseClone = response.clone();
+            caches.open("v1").then((cache) => {
+              cache.put(event.request, responseClone);
+            });
+            return response;
+          })
+          .catch(() => caches.match(`${path}/images/leaves.jpg`));
+      }
+    })
+  );
+});
