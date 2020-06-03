2420e76cd9a93e2a165948802d49dfa52a2b24c2
Andy Jakubowski <hello@andyjakubowski.com>
2020-06-03 16:29:23 +0200

    Stop caching app.js with Service Worker

--- serviceWorker.js
+++ serviceWorker.js
@@ -1,51 +1,61 @@
-const path = "/service-worker-demo";
+const path = '/service-worker-demo';
 
-self.addEventListener("install", (event) => {
-  console.log("[🤖SW] install fired.");
+// sdfsdlkj
+
+self.addEventListener('install', (event) => {
+  console.log('[🤖SW] install fired.');
   event.waitUntil(
-    caches.open("v1").then((cache) => {
+    caches.open('v1').then((cache) => {
       const items = [
         `${path}/`,
         `${path}/index.html`,
         `${path}/styles.css`,
-        `${path}/app.js`,
         `${path}/images/sunflower.jpg`,
         `${path}/images/icyWaterfall.jpg`,
         `${path}/images/cherryBlossom.jpg`,
         `${path}/images/leaves.jpg`,
       ];
-      console.log("Opened cache. Adding items:", items);
+      console.log('Opened cache. Adding items:', items);
       return cache.addAll(items);
     })
   );
 });
 
-self.addEventListener("activate", (event) => {
-  console.log("[🤖SW] activate fired.");
+self.addEventListener('activate', (event) => {
+  console.log('[🤖SW] activate fired.');
 });
 
-self.addEventListener("message", (event) => {
-  console.log("[🤖SW] message fired.");
+self.addEventListener('message', (event) => {
+  console.log('[🤖SW] message fired.');
   console.log(e);
 });
 
-self.addEventListener("fetch", (event) => {
-  console.log("[🤖SW] fetch fired:", event.request.url);
+self.addEventListener('fetch', (event) => {
+  console.log('[🤖SW] fetch fired:', event.request.url);
   event.respondWith(
-    caches.match(event.request).then((response) => {
-      if (response) {
-        return response;
-      } else {
-        return fetch(event.request)
-          .then((response) => {
-            const responseClone = response.clone();
-            caches.open("v1").then((cache) => {
-              cache.put(event.request, responseClone);
-            });
-            return response;
-          })
-          .catch(() => caches.match(`${path}/images/leaves.jpg`));
-      }
-    })
+    caches
+      .match(event.request)
+      .then((response) => response || fetch(event.request))
   );
 });
+
+// self.addEventListener('fetch', (event) => {
+//   console.log('[🤖SW] fetch fired:', event.request.url);
+//   event.respondWith(
+//     caches.match(event.request).then((response) => {
+//       if (response) {
+//         return response;
+//       } else {
+//         return fetch(event.request)
+//           .then((response) => {
+//             const responseClone = response.clone();
+//             caches.open('v1').then((cache) => {
+//               cache.put(event.request, responseClone);
+//             });
+//             return response;
+//           })
+//           .catch(() => caches.match(`${path}/images/leaves.jpg`));
+//       }
+//     })
+//   );
+// });
